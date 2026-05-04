946decd089e47406b93067884908630d1ef22d32
Ivison Filho <ivisoncaufilho@gmail.com>
2026-05-04 17:20:47 -0300

    feat:Consultar Saldo #4

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,24 +1,67 @@
 #include <iostream>
+#include <map>
 #include "../../include/model/Conta.h"
 
 using namespace std;
 
 int main() {
-    int numero;
+    map<int, Conta> contas;
+    int opcao;
 
-    cout << "Digite o numero da conta: ";
-    cin >> numero;
+    do {
+        cout << "\n1 - Criar conta\n2 - Consultar saldo\n0 - Sair\n";
+        cin >> opcao;
 
-    if (numero <= 0) {
-        cout << "Numero invalido!" << endl;
-        return 1;
-    }
+        int numero;
 
-    Conta conta(numero);
+        switch (opcao) {
 
-    cout << "Conta criada com sucesso!" << endl;
-    cout << "Numero: " << conta.getNumero() << endl;
-    cout << "Saldo: " << conta.getSaldo() << endl;
+            case 1: {
+                cout << "Digite o numero da conta: ";
+                cin >> numero;
+
+                if (numero <= 0) {
+                    cout << "Numero invalido!" << endl;
+                    break;
+                }
+
+                if (contas.find(numero) != contas.end()) {
+                    cout << "Conta ja existe!" << endl;
+                } else {
+                    contas.emplace(numero, Conta(numero));
+                    cout << "Conta criada com sucesso!" << endl;
+                }
+                break;
+            }
+
+            case 2: {
+                cout << "Digite o numero da conta: ";
+                cin >> numero;
+
+                if (numero <= 0) {
+                    cout << "Numero invalido!" << endl;
+                    break;
+                }
+
+                auto it = contas.find(numero);
+
+                if (it == contas.end()) {
+                    cout << "Conta nao encontrada!" << endl;
+                } else {
+                    cout << "Saldo: " << it->second.getSaldo() << endl;
+                }
+                break;
+            }
+
+            case 0:
+                cout << "Saindo..." << endl;
+                break;
+
+            default:
+                cout << "Opcao invalida!" << endl;
+        }
+
+    } while (opcao != 0);
 
     return 0;
 }
