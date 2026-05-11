621d04ede05f16afc0d6f085e2bf2e03b1d4ae6c
Ivison Filho <ivisoncaufilho@gmail.com>
2026-05-11 19:15:10 -0300

    feat: implementa conta bônus e lógica de pontuação #13

--- include/business/Banco.h
+++ include/business/Banco.h
@@ -6,10 +6,13 @@
 
 class Banco {
 private:
-    std::map<int, Conta> contas;
+    std::map<int, Conta*> contas;
 
 public:
+    ~Banco();
+    
     void criarConta(int numero);
+    void criarContaBonus(int numero);
     Conta* buscarConta(int numero);
 
     void creditar(int numero, double valor);
